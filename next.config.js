add5b30c1ed06f20929ceb980cc702e7b430c7fa
Joe <joe@Joes-MacBook-Pro-2.local>
2022-07-08 12:41:00 +0100

    init

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   reactStrictMode: true,
+  experimental: {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: '*',
+        },
+      ],
+    },
+  },
 }
 
 module.exports = nextConfig
---
+++ pages/articles/index.tsx
@@ -0,0 +1,61 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+interface IArticle {
+  title: string;
+  description: string;
+  source: { name: string }
+  url: string;
+  urlToImage: string | null;
+}
+
+interface IPageData {
+  data: { articles: IArticle[]; }
+}
+
+const Articles: NextPage<IPageData> = ({ data }) => {
+  return (
+    <div>
+      <Head>
+        <title>Top 5 Stories today</title>
+        <meta name="description" content="Top 5 articles right now" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <h1>Top 5 Stories today</h1>
+        <ul>
+          {data.articles.map(({ title, description, url, source, urlToImage }) => <li style={{ borderBottom: '1px solid lightgrey', paddingBottom: '1rem' }} key={title}>
+            <h2 >{title}</h2>
+            {urlToImage &&
+              <div style={{ width: '500px' }}>
+                <Image src={urlToImage} alt={title} width={300} height={200} layout='responsive' objectFit='contain' objectPosition='center' />
+              </div>
+            }
+            <p>{description}</p>
+            <a style={{ color: 'darkblue' }} href={url} rel="noreferrer" target="_blank">Visit {source.name} article here</a>
+          </li>)}
+        </ul>
+      </main>
+    </div>)
+}
+
+// This gets called on every request
+export const getServerSideProps = async () => {
+
+  const headers = new Headers();
+
+  headers.append('X-Api-Key', '54ddc98c540242b08f1a7a09c98d7999')
+
+  const Init = {
+    method: 'GET',
+    headers,
+  };
+
+  const res = await fetch(`https://newsapi.org/v2/top-headlines?country=gb&pageSize=5`, Init)
+  const data = await res.json()
+
+  return { props: { data } }
+}
+
+export default Articles
